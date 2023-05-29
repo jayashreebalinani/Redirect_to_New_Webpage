c5ec03321062c2952464b13ee9feaa6a1d29e6b0
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2023-05-29 21:10:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search)
 const token = urlParams.get('token')
 var newweb = window.location.href.split("token=")[1]
-sessionStorage.setItem('nextpage',true)
+sessionStorage.setItem('https://jayashreebalinani.github.io/Redirect_to_newwebpage/',true)
 if(sessionStorage.getItem("token") != token){
   //  window.location.href = "https://jayashreebalinani.github.io/Redirect_to_newwebpage/"
 }
